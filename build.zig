e2c9978e6668690163fad876d39b6dfc7a6b8e90
Fathony Teguh Irawan <4829378+fathonyfath@users.noreply.github.com>
2024-12-16 19:21:07 +0900

    Move sqlite source to lib

--- build.zig
+++ build.zig
@@ -26,24 +26,19 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 
-    const module = b.addModule("sqlite", .{
+    _ = b.addModule("sqlite", .{
         .root_source_file = b.path("src/sqlite.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    module.addIncludePath(b.path("src"));
-    module.addCSourceFiles(.{
-        .root = b.path("src"),
-        .files = &.{"sqlite3.c"},
-    });
 }
 
 fn addSqliteDependencies(b: *std.Build, compile: *std.Build.Step.Compile) void {
     compile.linkLibC();
-    compile.addIncludePath(b.path("src"));
+    compile.addIncludePath(b.path("lib"));
     compile.addCSourceFiles(.{
-        .root = b.path("src"),
+        .root = b.path("lib"),
         .files = &.{"sqlite3.c"},
     });
 }
